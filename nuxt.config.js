7713c130061adf7d4b09ab33d1f48614f2d8f085
Minhal Zaidi <minhal.zaidi@student.uts.edu.au>
2022-08-31 16:22:23 +1000

    Trying something else

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from '@nuxt/bridge'
 export default defineNuxtConfig({
     // Target: https://go.nuxtjs.dev/config-target
     target: 'static',
+    ssr: true,
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {

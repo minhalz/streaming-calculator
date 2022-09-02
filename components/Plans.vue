3a1a86b4ffa1359a57891e6d69753723ee0e1577
Minhal Zaidi <minhal.zaidi@student.uts.edu.au>
2022-09-02 15:56:32 +1000

    Remove unnecessary prop

--- components/Plans.vue
+++ components/Plans.vue
@@ -9,13 +9,6 @@ watch(amount, (newAmount) => {
 </script>
 <template>
     <ul class="absolute bottom-4 left-0 right-0 flex justify-center">
-        <Plan
-            v-for="{ id, label, cost } in plans"
-            :key="id"
-            v-model.number="amount"
-            :label="label"
-            :cost="cost"
-            :amount="amount"
-        />
+        <Plan v-for="{ id, label, cost } in plans" :key="id" v-model.number="amount" :label="label" :cost="cost" />
     </ul>
 </template>
